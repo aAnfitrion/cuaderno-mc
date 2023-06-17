605919a2565a47653c7ffc5187256aea2daf836e
aAnfitrion <tendondevastador@hotmail.com>
2023-06-17 15:00:43 -0500

    Agregamos un nuevo objeto a la lista de objetos

--- src/lib/objetos.ts
+++ src/lib/objetos.ts
@@ -3,6 +3,12 @@ export const objetos = [
 		id: 'tronco_de_roble',
 		nombre: 'Tronco de roble',
 		rareza: 1,
-		valor: 3
+		valor: 1
+	},
+	{
+		id: 'tronco_de_roble_oscuro',
+		nombre: 'Tronco de roble oscuro',
+		rareza: 2,
+		valor: 2
 	}
 ];
