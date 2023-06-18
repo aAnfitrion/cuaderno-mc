4cc6d646ad088b951110fd6603a3f6c3efc9b6bd
aAnfitrion <tendondevastador@hotmail.com>
2023-06-18 02:56:09 -0500

    feat: :sparkles: Añadimos la receta del pico de piedra, el hacha de piedra y la espada de piedra

--- src/lib/handleCraft.ts
+++ src/lib/handleCraft.ts
@@ -9,14 +9,41 @@ export function handleCraft(id: string, arr: any) {
 			restarObjetos(arr, 'tronco_de_roble', 2);
 			handle(arr, 'hacha_de_madera', 1);
 		}
-  }
-  if (id === 'pico_de_madera') {
+	}
+	if (id === 'pico_de_madera') {
 		const v1 = encontrarCantidad(arr, 'tronco_de_roble_oscuro', 3);
 		const v2 = encontrarCantidad(arr, 'tronco_de_roble', 2);
 		if (v1 && v2) {
 			restarObjetos(arr, 'tronco_de_roble_oscuro', 3);
 			restarObjetos(arr, 'tronco_de_roble', 2);
 			handle(arr, 'pico_de_madera', 1);
+		}
+	}
+	if (id === 'hacha_de_piedra') {
+		const v1 = encontrarCantidad(arr, 'roca', 3);
+		const v2 = encontrarCantidad(arr, 'tronco_de_roble', 2);
+		if (v1 && v2) {
+			restarObjetos(arr, 'roca', 3);
+			restarObjetos(arr, 'tronco_de_roble', 2);
+			handle(arr, 'hacha_de_piedra', 1);
+		}
+	}
+	if (id === 'pico_de_piedra') {
+		const v1 = encontrarCantidad(arr, 'roca', 3);
+		const v2 = encontrarCantidad(arr, 'tronco_de_roble', 2);
+		if (v1 && v2) {
+			restarObjetos(arr, 'roca', 3);
+			restarObjetos(arr, 'tronco_de_roble', 2);
+			handle(arr, 'pico_de_piedra', 1);
+		}
+	}
+	if (id === 'espada_de_piedra') {
+		const v1 = encontrarCantidad(arr, 'roca', 2);
+		const v2 = encontrarCantidad(arr, 'tronco_de_roble', 1);
+		if (v1 && v2) {
+			restarObjetos(arr, 'roca', 2);
+			restarObjetos(arr, 'tronco_de_roble', 1);
+			handle(arr, 'espada_de_piedra', 1);
 		}
 	}
 }
